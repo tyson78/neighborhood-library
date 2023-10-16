7814e7978fc7e8a0093f278dabe769e530755807
Tyson Hall <tn8938@gmail.com>
2023-10-16 19:06:06 -0400

    initial commit

--- src/main/java/org/pluralsight/Book.java
+++ src/main/java/org/pluralsight/Book.java
@@ -3,6 +3,15 @@ package org.pluralsight;
 class Book {
     int id;
     String title;
+
+    public Book(int id, String title, String isbn, boolean isCheckedOut, String checkedOutTo) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
     String isbn;
     boolean isCheckedOut;
     String checkedOutTo;
--- src/main/java/org/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/org/pluralsight/NeighborhoodLibrary.java
@@ -4,12 +4,33 @@ package org.pluralsight;
 // then press Enter. You can now see whitespace characters in your code.
 class NeighborhoodLibrary {
 
-    static Book[] books;
+    static Book[] books = {
+            new Book(1,"1Q84", "isbn", false, " ")
+    };
 
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.printf("%10.2f \n", 120.1);
+
+        String s = "hello , world";
+        String[] strings = s.split(",");
+        // strings = {"hello "," world"}
+
+        // "hello"
+        for (String s1 : strings) {
+            StringBuilder sb = new StringBuilder(s1.trim()); // "hello"
+            sb.replace(0, 1, sb.substring(0,1).toUpperCase()); //HELL
+            s1 = sb.toString();
+            System.out.println(s1);
+            //System.out.println(s1.trim().replaceFirst(s1.trim().substring(0,1), s1.trim().substring(0,1).toUpperCase()));
+        }
+
+        for (Book b : books) {
+            System.out.println(b.title);
+        }
+
 
     }
 }
